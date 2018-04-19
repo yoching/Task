b5295ec1d91ffdd55b06708473e902a716f6a990
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2018-04-19 19:04:44 +0900

    Add project

---
+++ Task/Task.h
@@ -0,0 +1,19 @@
+//
+//  Task.h
+//  Task
+//
+//  Created by Yoshikuni Kato on 4/19/18.
+//  Copyright © 2018 Yoshikuni Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Task.
+FOUNDATION_EXPORT double TaskVersionNumber;
+
+//! Project version string for Task.
+FOUNDATION_EXPORT const unsigned char TaskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Task/PublicHeader.h>
+
+
